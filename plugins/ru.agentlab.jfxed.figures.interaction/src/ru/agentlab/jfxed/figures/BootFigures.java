7d83a9b4ee7a21a95901c413b3acf6272418604e
alderik999 <alderik999@gmail.com>
2014-03-06 12:00:31 +0400

    Test commit

---
+++ plugins/ru.agentlab.jfxed.figures.interaction/src/ru/agentlab/jfxed/figures/BootFigures.java
@@ -0,0 +1,71 @@
+package ru.agentlab.jfxed.figures;
+
+import ru.agentlab.jfxed.figures.clazz.ClassFigure;
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class BootFigures extends Application {
+	
+	ClassFigure sourceToMove;
+	
+	double sceneStartX;
+	double sceneStartY;
+	
+	double figureStartTranslateX;
+	double figureStartTranslateY;
+	
+	
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.setTitle("Hello World!");
+
+		AnchorPane root = new AnchorPane();
+		
+		final ClassFigure source = new ClassFigure();
+		root.getChildren().add(source.getRoot());
+		root.getChildren().add(new VBox());
+		
+		Scene scene = new Scene(root, 800, 600);
+		//scene.getStylesheets().addAll(ClassFigure.class.getResource("ClassFigure.css").toExternalForm());
+		
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
+		
+		/*source.getRoot().setOnMousePressed(new EventHandler<MouseEvent>() {
+
+	        @Override
+	        public void handle(MouseEvent event) {
+	            System.out.println("setOnMousePressed");
+	            sceneStartX = event.getSceneX();
+	            sceneStartY = event.getSceneY();
+	            figureStartTranslateX = ((Node) event.getSource()).getTranslateX();
+	            figureStartTranslateY = ((Node) event.getSource()).getTranslateY();
+	        }
+	    });
+		
+		
+		source.getRoot().setOnMouseDragged(new EventHandler<MouseEvent>() {
+
+	        @Override
+	        public void handle(MouseEvent event) {
+	        	System.out.println("setOnMouseDragged");
+	        	System.out.println(event.getSceneX() + " " + event.getSceneY());
+	        	source.getRoot().setTranslateX(figureStartTranslateX + event.getSceneX() - sceneStartX);
+	        	source.getRoot().setTranslateY(figureStartTranslateY + event.getSceneY() - sceneStartY);
+	        }
+	    });*/
+
+	}
+}
