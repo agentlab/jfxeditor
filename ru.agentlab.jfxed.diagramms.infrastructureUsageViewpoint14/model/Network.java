8ba462bca65b004c903cd3be93d35285655b7ce4
darkbrightness <darkbrightness111@gmail.com>
2014-03-20 13:43:43 +0400

    Stas project

---
+++ ru.agentlab.jfxed.diagramms.infrastructureUsageViewpoint14/model/Network.java
@@ -0,0 +1,13 @@
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.vocabulary.VCARD;
+
+
+public class Network {
+	
+	public static void main (String[] args) {
+
+	}
+
+}
---
+++ ru.agentlab.jfxed.diagramms.infrastructureUsageViewpoint14/model/Xtend/Model.xtend
@@ -0,0 +1,5 @@
+package Xtend
+
+class Model {
+	
+}
