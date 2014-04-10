d298ab9d8066e971f40940c0e04c707e5820a109
student <student@lab2n08.iu3.bmstu.ru>
2014-04-10 14:32:23 +0500

    Shorokh UMLUseCase

--- plugins/ru.agentlab.jfxed.diagramms.UMLUseCase/src/ru/agentlab/jfxed/diagramms/UMLUseCase/UMLUseCase.java
+++ plugins/ru.agentlab.jfxed.diagramms.UMLUseCase/src/ru/agentlab/jfxed/diagramms/UMLUseCase/UMLUseCase.java
@@ -1,4 +1,5 @@
 package ru.agentlab.jfxed.diagramms.UMLUseCase;
+
 import com.hp.hpl.jena.ontology.Individual;
 import com.hp.hpl.jena.ontology.ObjectProperty;
 import com.hp.hpl.jena.ontology.OntClass;
@@ -8,37 +9,68 @@ import com.hp.hpl.jena.rdf.model.ModelFactory;
 public class UMLUseCase {
 
 	static String SOURCE = "http://www.eswc2006.org/technologies/ontology";
-static String NS = SOURCE + "#";
+	static String NS = SOURCE + "#";
 
-//test from sourcetree
-public static void main (String[] args){
-OntModel m = ModelFactory.createOntologyModel();//ñîçäàåòñÿ ïóñòàÿ áàçà çíàíèé
-OntClass UseCaseClass = m.createClass(NS + "Use Case");// блок Use Case triple
-OntClass actorClass = m.createClass(NS + "Actor");// actor
-OntClass associationClass = m.createClass(NS + "Association");// связь Association
-OntClass includeClass = m.createClass(NS + "Include");// связь Include
-OntClass extendClass = m.createClass(NS + "Extend");// связь Extend
+	// test from sourcetree
+	public static void main(String[] args) {
+		OntModel m = ModelFactory.createOntologyModel();// пустая база знаний
+		OntClass actorClass = m.createClass(NS + "Actor");// actor
+		OntClass usecaseClass = m.createClass(NS + "Use Case");// блок Use Case
+		OntClass atmClass = m.createClass(NS + "ATM");// ATM
+		OntClass associationClass = m.createClass(NS + "Association");// связь Association
+		OntClass inclusionClass = m.createClass(NS + "Include");// связь Include
+		OntClass extensionClass = m.createClass(NS + "Extend");// связь Extend
 
+		OntClass aggregationClass = m.createClass(NS + "Agregation");// связь
+		aggregationClass.addSuperClass(associationClass);// родительская связь
 
-OntClass aggregationClass = m.createClass(NS + "Agregation");//ñâÿçü
-aggregationClass.addSuperClass(associationClass);//ðîäèòåëüñêàÿ ñâÿçü
+		ObjectProperty aggrPropTo = m.createObjectProperty(NS + "AggregationTo");// объекты - свойства
+		aggrPropTo.addDomain(aggregationClass);// свойства у aggregation
+		aggrPropTo.addRange(usecaseClass);
+		ObjectProperty aggrPropFrom = m.createObjectProperty(NS + "AggregationFrom");
+		aggrPropFrom.addDomain(aggregationClass);
+		aggrPropFrom.addRange(aggregationClass);
 
-ObjectProperty propTo = m.createObjectProperty(NS + "To");//îáúåêòû - ñâîéñòâà
-ObjectProperty propFrom = m.createObjectProperty(NS + "From");
-
-propTo.addDomain(aggregationClass);//ñâîéñòâà ó aggregation
-propFrom.addDomain(aggregationClass);
-
-propTo.addRange(contractClass);//òî ÷òî ìîãóò ïðèíèìàòü
-propFrom.addRange(productClass);
-
-//íàïîëíåíèå áàçû
-Individual productIndividual = m.createIndividual(NS + "jkljklj2", productClass);
-Individual contractIndividual = m.createIndividual(NS + "jkljklj3", contractClass);
-
-Individual aggregationIndividual = m.createIndividual(NS + "jkljklj1", aggregationClass);
-aggregationIndividual.addProperty(propTo, contractIndividual);//îò ïðîäóêòà ê êîíòðàêòó
-aggregationIndividual.addProperty(propFrom, productIndividual);
-m.write(System.out);//è â êîíñîëü
-}
+		// связь actor - use case
+		ObjectProperty assocPropTo = m.createObjectProperty(NS + "AssociationTo");
+		assocPropTo.addRange(actorClass);
+		assocPropTo.addDomain(associationClass);
+		ObjectProperty assocPropFrom = m.createObjectProperty(NS + "AssociationFrom");
+		assocPropFrom.addDomain(associationClass);
+		assocPropFrom.addRange(usecaseClass);
+		
+		// связи include
+		ObjectProperty incPropTo = m.createObjectProperty(NS + "InclusionTo");
+		incPropTo.addDomain(inclusionClass);
+		incPropTo.addRange(usecaseClass);
+		ObjectProperty incPropFrom = m.createObjectProperty(NS + "InclusionFrom");
+		incPropFrom.addRange(usecaseClass);
+		incPropFrom.addDomain(inclusionClass);
+		
+		// связи extend
+		ObjectProperty extPropTo = m.createObjectProperty(NS + "ExtensionTo");
+		extPropTo.addDomain(extensionClass);
+		extPropTo.addRange(usecaseClass);
+		ObjectProperty extPropFrom = m.createObjectProperty(NS + "ExtensionFrom");
+		extPropFrom.addRange(usecaseClass);
+		extPropFrom.addDomain(extensionClass);
+		
+		// наполнение базы
+		Individual user = m.createIndividual(NS + "User", actorClass);
+		Individual withdrawCash = m.createIndividual(NS + "Withdraw cash", usecaseClass);
+		Individual checkBalance = m.createIndividual(NS + "Check balance", usecaseClass);
+		Individual donate = m.createIndividual(NS + "Donate money to charity", usecaseClass);
+		Individual transferMoney = m.createIndividual(NS + "Transfer money", usecaseClass);
+		Individual payBills = m.createIndividual(NS + "Pay bills", usecaseClass);
+		Individual login = m.createIndividual(NS + "Extension poits. Invalid password entered. Abort triggered", usecaseClass);
+		Individual handleInvalidPassword = m.createIndividual(NS + "Handle invalid password", usecaseClass);
+		Individual handleAbort = m.createIndividual(NS + "Handle abort", usecaseClass);
+		Individual atm = m.createIndividual(NS + "ATM", atmClass);
+		
+		
+		Individual aggregationIndividual = m.createIndividual(NS + "agr1", aggregationClass);
+		//aggregationIndividual.addProperty(aggrPropTo, contractIndividual);// îò ïðîäóêòà ê êîíòðàêòó
+		//aggregationIndividual.addProperty(propFrom, productIndividual);
+		m.write(System.out);// в консоль
+	}
 }
