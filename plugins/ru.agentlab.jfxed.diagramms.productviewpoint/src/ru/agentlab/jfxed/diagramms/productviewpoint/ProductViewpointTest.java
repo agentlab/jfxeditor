7521a31c80c41344d351cd7956a7c568661ef1ae
Ilya Gutnikov <alderik999@gmail.com>
2014-06-15 12:46:11 +0400

    Магия 3 лабы.

--- plugins/ru.agentlab.jfxed.diagramms.productviewpoint/src/ru/agentlab/jfxed/diagramms/productviewpoint/ProductViewpointTest.java
+++ plugins/ru.agentlab.jfxed.diagramms.productviewpoint/src/ru/agentlab/jfxed/diagramms/productviewpoint/ProductViewpointTest.java
@@ -3,7 +3,13 @@ import com.hp.hpl.jena.ontology.Individual;
 import com.hp.hpl.jena.ontology.ObjectProperty;
 import com.hp.hpl.jena.ontology.OntClass;
 import com.hp.hpl.jena.ontology.OntModel;
+import com.hp.hpl.jena.query.Query;
+import com.hp.hpl.jena.query.QueryExecution;
+import com.hp.hpl.jena.query.QueryExecutionFactory;
+import com.hp.hpl.jena.query.QueryFactory;
+import com.hp.hpl.jena.query.ResultSetFormatter;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.StmtIterator;
 
 public class ProductViewpointTest {
 
@@ -24,25 +30,47 @@ public class ProductViewpointTest {
 		/**
 		 * Вершины графа
 		 */
+		OntClass concept = m.createClass(NS + "Concept");
 		
 		OntClass productClass = m.createClass(NS + "Product");//создаем отдельные triple
+		productClass.addSuperClass(concept);
+		
 		OntClass contractClass = m.createClass(NS + "Contract");
+		contractClass.addSuperClass(concept);
+		
 		OntClass businessServiceClass = m.createClass(NS + "Business Service");
+		businessServiceClass.addSuperClass(concept);
+		
 		OntClass valueClass = m.createClass(NS + "Value");
+		valueClass.addSuperClass(concept);
+		
 		OntClass businessInterfaceClass = m.createClass(NS + "Business interface");
-		OntClass businessEventClass = m.createClass(NS + "Business event");
+		businessInterfaceClass.addSuperClass(concept);
+		
+		OntClass businessEventClass = m.createClass(NS + "BusinessEvent");
+		businessEventClass.addSuperClass(concept);
+		
 		OntClass businessProcessClass = m.createClass(NS + "Business process");
+		businessProcessClass.addSuperClass(concept);
+		
 		OntClass businessRoleClass = m.createClass(NS + "Business role");
+		businessRoleClass.addSuperClass(concept);
+		
 		OntClass businessActorClass = m.createClass(NS + "Business actor");
+		businessActorClass.addSuperClass(concept);
 		
 		OntClass applicationServiceClass = m.createClass(NS + "Application service");
+		applicationServiceClass.addSuperClass(concept);
+		
 		OntClass applicationInterfaceClass = m.createClass(NS + "Application interface");
+		applicationInterfaceClass.addSuperClass(concept);
+		
 		OntClass applicationComponentClass = m.createClass(NS + "Application component");
+		applicationComponentClass.addSuperClass(concept);
 		
 		/*
 		 * end
 		 * */
-		
 		
 		/*
 		 * Объявление связей
@@ -58,11 +86,11 @@ public class ProductViewpointTest {
 		OntClass compositionClass = m.createClass(NS + "Composition");//composition
 		OntClass triggeringClass = m.createClass(NS + "Triggering");//triggering
 		OntClass accessClass = m.createClass(NS + "Composition");//composition
-		OntClass realizationClass = m.createClass(NS + "CRealization");
+		OntClass realizationClass = m.createClass(NS + "Realization");
 		
 		//сюда повсему мы говорим что наши стрелочки это association
 		aggregationClass.addSuperClass(associationClass);//родительская связь
-		assigmentClass.addSuperClass(assigmentClass);//родительская связь
+		assigmentClass.addSuperClass(associationClass);//родительская связь
 		usedByClass.addSuperClass(associationClass);//родительская связь
 		compositionClass.addSuperClass(associationClass);//родительская связь
 		triggeringClass.addSuperClass(associationClass);//родительская связь
@@ -71,10 +99,17 @@ public class ProductViewpointTest {
 		/*
 		 * end
 		 * */
+	    
+		//propTo.addDomain(aggregationClass);//свойства у aggregation
+		//propFrom.addDomain(aggregationClass);
 		
-		propTo.addDomain(aggregationClass);//свойства у aggregation
-		propFrom.addDomain(aggregationClass);
+		propTo.addDomain(associationClass);
+		propTo.addRange(valueClass);
 		
+		propFrom.addDomain(valueClass);
+		propFrom.addRange(associationClass);
+		
+		/*
 		//связи в
 		propFrom.addRange(productClass);//продукт содержит конракт т.е. связь между ними инвертировать связи
 		propFrom.addRange(valueClass);
@@ -100,7 +135,8 @@ public class ProductViewpointTest {
 		propTo.addRange(applicationServiceClass);
 		propTo.addRange(applicationInterfaceClass);
 		propTo.addRange(applicationComponentClass);
-		
+		*/
+
 		
 		//наполнение базы создание экзампляров
 		Individual productIndividual = m.createIndividual(NS + "product", productClass);
@@ -116,6 +152,7 @@ public class ProductViewpointTest {
 		Individual applicationInterfaceIndividual = m.createIndividual(NS+ "applicationInterface", applicationInterfaceClass);
 		Individual applicationComponentIndividual = m.createIndividual(NS+ "applicationComponent", applicationComponentClass);
 		
+		
 		//связи
 		Individual aggToPFromCo_BS_ASIndividual = m.createIndividual(NS + "aggToProd_FromCo_BS_AS", aggregationClass);
 		aggToPFromCo_BS_ASIndividual.addProperty(propFrom, productIndividual);
@@ -123,17 +160,20 @@ public class ProductViewpointTest {
 		aggToPFromCo_BS_ASIndividual.addProperty(propTo, businessServiceIndividual);//делаем связку
 		aggToPFromCo_BS_ASIndividual.addProperty(propTo, applicationServiceIndividual);
 		
+		
 		Individual assocValueProduct = m.createIndividual(NS + "assocValueProduct", associationClass);
 		assocValueProduct.addProperty(propFrom, valueIndividual);
 		assocValueProduct.addProperty(propTo, valueIndividual);
 		assocValueProduct.addProperty(propFrom, productIndividual);
 		assocValueProduct.addProperty(propTo, productIndividual);
 		
+		/*
 		Individual assigmBusActorBusRole = m.createIndividual(NS + "assigmentBusinessRole_BusinessActor", assigmentClass);
 		assigmBusActorBusRole.addProperty(propTo, businessRoleIndividual);
 		assigmBusActorBusRole.addProperty(propTo, businessActorIndividual);
 		assigmBusActorBusRole.addProperty(propFrom, businessRoleIndividual);
 		assigmBusActorBusRole.addProperty(propFrom, businessActorIndividual);
+		
 		
 		Individual assigmBusRoleBusProc = m.createIndividual(NS + "assigmBusRoleBusProc", assigmentClass);
 		assigmBusRoleBusProc.addProperty(propTo, businessRoleIndividual);
@@ -213,6 +253,58 @@ public class ProductViewpointTest {
 		trigBusProcBusEvent.addProperty(propTo, businessEventIndividual);
 		trigBusProcBusEvent.addProperty(propFrom, businessProcessIndividual);
 		
-		m.write(System.out, "N3");//и в консоль
+		*/
+		
+		m.write(System.out, "RDF/XML");//и в консоль
+		//m.containsAny((StmtIterator) businessEventIndividual);
+		
+		//m.read(SOURCE, "RDF/XML" );
+		/*
+  <rdf:Description rdf:about="http://www.eswc2006.org/technologies/ontology#assigmentBusinessRole_BusinessActor">
+    <j.1:From rdf:resource="http://www.eswc2006.org/technologies/ontology#businessActor"/>
+    <j.1:From rdf:resource="http://www.eswc2006.org/technologies/ontology#businessRole"/>
+    <j.1:To rdf:resource="http://www.eswc2006.org/technologies/ontology#businessActor"/>
+    <j.1:To rdf:resource="http://www.eswc2006.org/technologies/ontology#businessRole"/>
+    <rdf:type rdf:resource="http://www.eswc2006.org/technologies/ontology#Assigment"/>
+  </rdf:Description>
+		 * */
+/*
+		String queryString =        
+			        "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>  "
+			        + "select ?uri "+
+			        "where { "+
+			         "?uri rdf:type <http://www.eswc2006.org/technologies/ontology#Assigment>  "+
+			        "} \n ";
+			    Query query = QueryFactory.create(queryString);
+		
+			    // Execute the query and obtain results
+			    QueryExecution qe = QueryExecutionFactory.create(query, m);
+			    com.hp.hpl.jena.query.ResultSet results =  qe.execSelect();
+
+			    // Output query results    
+			    ResultSetFormatter.out(System.out, results, query);
+			    
+			    qe.close();
+			    */
+		/*
+<rdf:Description rdf:about="http://www.eswc2006.org/technologies/ontology#businessEvent">
+    <rdf:type rdf:resource="http://www.eswc2006.org/technologies/ontology#BusinessEvent"/>
+  </rdf:Description>*/
+				String queryString2 =        
+				        "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>  "
+				        + "select ?uri "+
+				        "where { "+
+				         "?uri rdf:type <http://www.eswc2006.org/technologies/ontology#Product>  "+
+				        "} \n ";
+				    Query query2 = QueryFactory.create(queryString2);
+			
+				    // Execute the query and obtain results
+				    QueryExecution qe2 = QueryExecutionFactory.create(query2, m);
+				    com.hp.hpl.jena.query.ResultSet results2 =  qe2.execSelect();
+
+				    // Output query results    
+				    ResultSetFormatter.out(System.out, results2, query2);
+				    qe2.close();
+			    
 		}
 	}
