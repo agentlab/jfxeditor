dbfd45edccf8ed8f1133966726caa5498e0573ed
Ludmila <Ludmila@Ludmila_off>
2014-06-26 17:38:43 +0400

    лучи ненависти в небо. от неба лучи добра. дайте билееет!!!

--- plugins/ru.agentlab.umodeler.figures.representation/src/ru/agentlab/jfxed/figures/BootFigures.java
+++ plugins/ru.agentlab.umodeler.figures.representation/src/ru/agentlab/jfxed/figures/BootFigures.java
@@ -16,16 +16,13 @@ public class BootFigures extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		primaryStage.setTitle("Representation!");
-
+ 
 		AnchorPane root = new AnchorPane();
-
 		final Rectangle rect = new Rectangle();
-		final Text1 text1 = new Text1();
 		root.getChildren().add(rect.getRoot());
-		root.getChildren().add(text1.getRoot());
 		root.getChildren().add(new VBox());
 		
-		Scene scene = new Scene(root, 200, 150);
+		Scene scene = new Scene(root, 50, 150);
 		primaryStage.setScene(scene);
 		primaryStage.show();
 
